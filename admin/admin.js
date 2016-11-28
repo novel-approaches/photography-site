7924a42e844e475517a3a5ae338b576dc125175b
travis.h.oneill@gmail.com <travis.h.oneill@gmail.com>
2016-11-27 19:14:40 -0800

    feat(admin): add API object

--- admin/admin.js
+++ admin/admin.js
@@ -42,13 +42,49 @@
 /************************************************************************/
 /******/ ([
 /* 0 */
-/***/ function(module, exports) {
+/***/ function(module, exports, __webpack_require__) {
+
+	// import { API } from './api.js';
+	const API = __webpack_require__(1);
 
 	document.addEventListener('DOMContentLoaded', () => {
 	  let div = document.createElement('div');
 	  div.innerHTML = 'HELLO';
+	  console.log(API);
 	  document.body.appendChild(div);
 	});
+
+
+/***/ },
+/* 1 */
+/***/ function(module, exports) {
+
+	module.exports = {
+	  "development": {
+	    "cloud_name": "clairephotography",
+	    "api_key": "439198291915981",
+	    "api_secret": "Mkg4z0aSdewn1XlM79gNvxz1EwY",
+	    "enhance_image_tag": "true",
+	    "static_image_support": "false"
+	  },
+	  "production": {
+	    "cloud_name": "clairephotography",
+	    "api_key": "439198291915981",
+	    "api_secret": "Mkg4z0aSdewn1XlM79gNvxz1EwY",
+	    "enhance_image_tag": "true",
+	    "static_image_support": "false"
+	  },
+	  "test": {
+	    "cloud_name": "clairephotography",
+	    "api_key": "439198291915981",
+	    "api_secret": "Mkg4z0aSdewn1XlM79gNvxz1EwY",
+	    "enhance_image_tag": "true",
+	    "static_image_support": "false"
+	  },
+	  "env_var": "CLOUDINARY_URL=cloudinary://439198291915981:Mkg4z0aSdewn1XlM79gNvxz1EwY@clairephotography",
+	  "secure_delivery_url": "https://res.cloudinary.com/clairephotography/image/upload/",
+	  "API_base_url":  "https://api.cloudinary.com/v1_1/clairephotography/image/upload/"
+	}
 
 
 /***/ }
--- admin/admin_dev.js
+++ admin/admin_dev.js
@@ -1,5 +1,9 @@
+// import { API } from './api.js';
+const API = require('./api.js');
+
 document.addEventListener('DOMContentLoaded', () => {
   let div = document.createElement('div');
   div.innerHTML = 'HELLO';
+  console.log(API);
   document.body.appendChild(div);
 });
