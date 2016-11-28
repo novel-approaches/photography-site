08d14a74cd44de46a647b021f580139449155644
travis.h.oneill@gmail.com <travis.h.oneill@gmail.com>
2016-11-28 13:20:35 -0800

    fix(admin): Cloudinary uploads working

--- admin/admin.js
+++ admin/admin.js
@@ -107,82 +107,28 @@ module.exports = {
 
 var API = __webpack_require__(0);
 
-// window.ajaxSuccess = function () {
-// 	response = JSON.parse(this.responseText);
-//   console.log("ajaxSuccess", typeof this.responseText);
-//   document.getElementById('uploaded').setAttribute("src", response["secure_url"]);
-//   document.getElementById('results').innerText = this.responseText;
-// }
-//
-// window.AJAXSubmit = function (formElement) {
-//   console.log("starting AJAXSubmit");
-//   // if (!formElement.action) { return; }
-//   data = new FormData(formElement);
-//   debugger;
-//   $.ajax({
-//     url: API.base_url,
-//     type: 'POST',
-//     data: JSON.stringify(data),
-//     sucess(res){
-//       console.log(res);
-//     }
-//   });
-
-// var xhr = new XMLHttpRequest();
-// xhr.onload = ajaxSuccess;
-// xhr.open("post", "https://api.cloudinary.com/v1_1/clairephotography/image/upload");
-// xhr.send(new FormData(formElement));
-// }
-
-
-function res() {
-  console.log(response);
+function ajaxSuccess() {
+  var res = JSON.parse(this.response);
+  console.log(res.secure_url);
 }
 
 function postImage(img) {
-  debugger;
   var data = new FormData();
   data.append('upload_preset', 'standard');
   data.append('file', img);
   data.append('api_key', API.development.api_key);
-  // data = JSON.stringify(data);
-  debugger;
-  // for(x of data.entries()){console.log(x);}
-  $.ajax({
-    url: API.base_url,
-    type: 'post',
-    data: data,
-    sucess: function sucess(res) {
-      console.log(res);
-    },
-    error: function error(err) {
-      console.log(err);
-    }
-  });
-  // var xhr = new XMLHttpRequest();
-  // xhr.onload = res;
-  // xhr.open("post", "API.base_url");
-  // xhr.send(new FormData(formElement));
+  var xhr = new XMLHttpRequest();
+  xhr.onload = ajaxSuccess;
+  xhr.open("post", API.base_url, true);
+  xhr.send(data);
+  console.log(xhr.responseText);
 }
 
-// function read(file){
-//   var reader = new FileReader()
-//   reader.onload = function(){
-//     console.log(event.target);
-//     let binary = event.target.result
-//     postImage(binary);
-//   }
-//   reader.readAsArrayBuffer(file);
-// }
-
 function handleUpload(event) {
-  // event.preventDefault();
   var files = event.target.files;
   for (var i = 0; i < files.length; i++) {
     postImage(files[i]);
-    // postImage(files[i]);
   }
-  // debugger;
 }
 
 document.addEventListener('DOMContentLoaded', function () {
--- admin/admin_dev.js
+++ admin/admin_dev.js
@@ -1,84 +1,28 @@
 'use strict';
 const API = require('./api');
 
-
-// window.ajaxSuccess = function () {
-// 	response = JSON.parse(this.responseText);
-//   console.log("ajaxSuccess", typeof this.responseText);
-//   document.getElementById('uploaded').setAttribute("src", response["secure_url"]);
-//   document.getElementById('results').innerText = this.responseText;
-// }
-//
-// window.AJAXSubmit = function (formElement) {
-//   console.log("starting AJAXSubmit");
-//   // if (!formElement.action) { return; }
-//   data = new FormData(formElement);
-//   debugger;
-//   $.ajax({
-//     url: API.base_url,
-//     type: 'POST',
-//     data: JSON.stringify(data),
-//     sucess(res){
-//       console.log(res);
-//     }
-//   });
-
-  // var xhr = new XMLHttpRequest();
-  // xhr.onload = ajaxSuccess;
-  // xhr.open("post", "https://api.cloudinary.com/v1_1/clairephotography/image/upload");
-  // xhr.send(new FormData(formElement));
-// }
-
-
-function res(){
-  console.log(response);
+function ajaxSuccess(){
+  let res = JSON.parse(this.response);
+  console.log(res.secure_url);
 }
 
 function postImage(img){
-  debugger;
-  let data = new FormData()
+  let data = new FormData();
   data.append('upload_preset', 'standard');
   data.append('file', img);
   data.append('api_key', API.development.api_key);
-  // data = JSON.stringify(data);
-  debugger;
-  // for(x of data.entries()){console.log(x);}
-  $.ajax({
-    url: API.base_url,
-    type: 'post',
-    data: data,
-    sucess(res){
-      console.log(res);
-    },
-    error(err){
-      console.log(err);
-    }
-  });
-  // var xhr = new XMLHttpRequest();
-  // xhr.onload = res;
-  // xhr.open("post", "API.base_url");
-  // xhr.send(new FormData(formElement));
+  let xhr = new XMLHttpRequest();
+  xhr.onload = ajaxSuccess;
+  xhr.open("post", API.base_url, true);
+  xhr.send(data);
+  console.log(xhr.responseText);
 }
 
-// function read(file){
-//   var reader = new FileReader()
-//   reader.onload = function(){
-//     console.log(event.target);
-//     let binary = event.target.result
-//     postImage(binary);
-//   }
-//   reader.readAsArrayBuffer(file);
-// }
-
 function handleUpload(event){
-  // event.preventDefault();
   let files = event.target.files;
   for (var i = 0; i < files.length; i++) {
-    postImage(files[i])
-    // postImage(files[i]);
+    postImage(files[i]);
   }
-  // debugger;
-
 }
 
 document.addEventListener('DOMContentLoaded', () => {
