5fbe940c938196ef64c5b12198d07bab0ed41a1c
Oliver Isenrich <IsenrichO@gmail.com>
2016-11-28 18:01:48 -0800

    refactor: Remove <header> element and delegate to Header component

--- frontend/app/App.jsx
+++ frontend/app/App.jsx
@@ -2,6 +2,23 @@
 import  React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
+import Header from './components/Header';
+
+
+const API_BASE = 'https://res.cloudinary.com/clairephotography/image/upload/',
+      IMAGE_URLS = [
+        'v1480368582/iq4sy5drl6viareno3hr.jpg',
+        'v1480367907/mmhqphjs5fohx0vljsjd.jpg',
+        'v1480367907/cvtavj9flxyvdadryjiq.gif',
+        'v1480367907/r3eqb3rulk5ksfkb2gmb.jpg',
+        'v1480367906/m35pyne0tfcho33xqznj.jpg',
+        'v1480367754/qconp93imcktgsfnii80.jpg',
+        'v1480367720/khjkrbqh8gpuyt3hdnzp.jpg',
+        'v1480367446/uaejtj7reyqupsccth7o.jpg',
+        'v1480367139/azkc56ixa5jmgmwie1gf.jpg',
+        'v1480366606/kl3f711j7nx4uk1jtyhg.gif'
+      ];
+
 
 export default class App extends Component {
 
@@ -12,9 +29,7 @@ export default class App extends Component {
   render() {
     return (
       <div>
-        <header onClick={this.handleClick}>
-          Welcome to Photo Album
-        </header>
+        <Header />
         {this.props.children}
       </div>
     );
