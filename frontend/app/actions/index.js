37cb1283e6775ae814491a2f92c01ab037286142
Oliver Isenrich <IsenrichO@gmail.com>
2016-11-30 22:37:56 -0800

    feat: Add Redux actions to register addToShoppingCart and selectPhoto

--- frontend/app/actions/index.js
+++ frontend/app/actions/index.js
@@ -5,6 +5,8 @@ export const MODIFY_GRID_MARGINS = 'MODIFY_GRID_MARGINS';
 export const MODIFY_GRID_SIZE = 'MODIFY_GRID_SIZE';
 export const ORDER_PHOTO = 'ORDER_PHOTO';
 export const TOGGLE_MODAL = 'TOGGLE_MODAL';
+export const ADD_TO_SHOPPING_CART = 'ADD_TO_SHOPPING_CART';
+export const SELECT_PHOTO = 'SELECT_PHOTO';
 
 
 export const modifyGridMargins = (margins) => ({
@@ -22,7 +24,17 @@ export const orderPhoto = (photo) => ({
   photo
 });
 
-// Action used to handle display toggle of Google Maps Modal box:
+// Action used to handle display toggle of OrderFormModal React container:
 export const toggleModal = () => ({
   type: TOGGLE_MODAL
+});
+
+export const addToShoppingCart = (payload) => ({
+  type: ADD_TO_SHOPPING_CART,
+  payload
+});
+
+export const selectPhoto = (photo) => ({
+  type: SELECT_PHOTO,
+  photo
 });
