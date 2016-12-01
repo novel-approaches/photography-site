99e358f91125988c3658ab5a76b63114d8817c87
Oliver Isenrich <IsenrichO@gmail.com>
2016-11-30 20:49:17 -0800

    feat: Add functionality for disabling OrderFormModal

--- frontend/app/containers/OrderFormModal.jsx
+++ frontend/app/containers/OrderFormModal.jsx
@@ -1,25 +1,52 @@
 'use strict';
 import React, { Component } from 'react';
 import Modal from 'react-modal';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+
+import { orderPhoto, toggleModal } from '../actions/index';
+import OrderFormModalStyles from '../constants/json/OrderFormModalStyles.json';
 
 
 class OrderFormModal extends Component {
   constructor(props) {
     super(props);
+    this.closeOrderFormModal = this.closeOrderFormModal.bind(this);
+  }
+
+  closeOrderFormModal(evt) {
+    this.props.toggleModal();
   }
 
   render() {
     return (
-      <div id="order-form">
-        <Modal />
-      </div>
+      <Modal
+        isOpen={ this.props.orderFormModal }
+        style={ OrderFormModalStyles }>
+        <h4>Your Order</h4>
+        <i
+          id="close-modal-btn"
+          onClick={ this.closeOrderFormModal }>
+          &times;
+        </i>
+      </Modal>
     );
   }
 };
 
 let mapStateToProps = (state) => ({
-
+  photoOrder: state.photoOrder,
+  orderFormModal: state.orderFormModal
 });
 
-// export default ThumbnailsMap;
-export default connect(mapStateToProps)(OrderFormModal);
+let mapDispatchToProps = (dispatch) => bindActionCreators({
+  orderPhoto,
+  toggleModal
+}, dispatch);
+
+export default connect(mapStateToProps, mapDispatchToProps)(OrderFormModal);
+
+
+// <div id="order-form"></div>
+
+// style={ customStyles }
