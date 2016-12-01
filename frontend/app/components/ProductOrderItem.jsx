9408a0ccaca2213cb6ec3b0fffd586e341b745b6
Oliver Isenrich <IsenrichO@gmail.com>
2016-12-01 04:04:36 -0800

    refactor: Rewrite as individual list items

--- frontend/app/components/ProductOrderItem.jsx
+++ frontend/app/components/ProductOrderItem.jsx
@@ -3,14 +3,14 @@ import React, { Component } from 'react';
 
 
 const ProductOrderItem = ({ item }) => (
-  <div
+  <li
     className="product-item">
     <h5>{ item.public_id }</h5>
     <img
       src={ item.secure_url }
       alt={ `Photograph thumbnail ${item.public_id}` }
       height="75px" />
-  </div>
+  </li>
 );
 
 export default ProductOrderItem;
