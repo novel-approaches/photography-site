6dccb15c625fe85ac8751ec0d04abd72a5089c84
Oliver Isenrich <IsenrichO@gmail.com>
2016-11-30 22:41:40 -0800

    chore: Accumulate shoppingCart and photoSelect reducers

--- frontend/app/reducers/rootReducer.js
+++ frontend/app/reducers/rootReducer.js
@@ -5,13 +5,17 @@ import gridMargins from './gridMargins_Reducer';
 import gridSize from './gridSize_Reducer';
 import photoOrder from './photoOrder';
 import orderFormModal from './orderFormModal_Reducer';
+import shoppingCart from './shoppingCart_Reducer';
+import photoSelect from './photoSelect_Reducer';
 
 
 const rootReducer = combineReducers({
   gridMargins,
   gridSize,
   photoOrder,
-  orderFormModal
+  orderFormModal,
+  shoppingCart,
+  photoSelect
 });
 
 export default rootReducer;
