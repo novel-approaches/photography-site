b7792c823dec248f4ec4bf30ce7b9a7f5b609310
Oliver Isenrich <IsenrichO@gmail.com>
2016-11-30 20:47:35 -0800

    feat: Add Redux action to register with toggling of OrderFormModal

--- frontend/app/actions/index.js
+++ frontend/app/actions/index.js
@@ -4,6 +4,7 @@
 export const MODIFY_GRID_MARGINS = 'MODIFY_GRID_MARGINS';
 export const MODIFY_GRID_SIZE = 'MODIFY_GRID_SIZE';
 export const ORDER_PHOTO = 'ORDER_PHOTO';
+export const TOGGLE_MODAL = 'TOGGLE_MODAL';
 
 
 export const modifyGridMargins = (margins) => ({
@@ -19,4 +20,9 @@ export const modifyGridSize = (size) => ({
 export const orderPhoto = (photo) => ({
   type: ORDER_PHOTO,
   photo
+});
+
+// Action used to handle display toggle of Google Maps Modal box:
+export const toggleModal = () => ({
+  type: TOGGLE_MODAL
 });
