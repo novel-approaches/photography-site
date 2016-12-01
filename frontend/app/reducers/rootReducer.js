a797d59ec4006488d8311b027a585000cf5c0ac4
Oliver Isenrich <IsenrichO@gmail.com>
2016-11-30 20:46:57 -0800

    chore: Accumulate orderFormModal in rootReducer

--- frontend/app/reducers/rootReducer.js
+++ frontend/app/reducers/rootReducer.js
@@ -4,12 +4,14 @@ import { combineReducers } from 'redux';
 import gridMargins from './gridMargins_Reducer';
 import gridSize from './gridSize_Reducer';
 import photoOrder from './photoOrder';
+import orderFormModal from './orderFormModal_Reducer';
 
 
 const rootReducer = combineReducers({
   gridMargins,
   gridSize,
-  photoOrder
+  photoOrder,
+  orderFormModal
 });
 
 export default rootReducer;
