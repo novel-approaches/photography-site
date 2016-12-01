43c479c20232e26c76fb4006652dafa3439546a2
Oliver Isenrich <IsenrichO@gmail.com>
2016-11-30 20:49:53 -0800

    feat: Add OrderFormModal as a custom component within Header

--- frontend/app/components/Header.jsx
+++ frontend/app/components/Header.jsx
@@ -1,11 +1,41 @@
 'use strict';
 import React, { Component } from 'react';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+
+import OrderFormModal from '../containers/OrderFormModal';
+import { toggleModal } from '../actions/index';
 
 
-const Header = () => (
-  <header>
-    Welcome to Photo Album
-  </header>
-);
+// const Header = () => (
+class Header extends Component {
+  constructor(props) {
+    super(props);
+    this.renderModal = this.renderModal.bind(this);
+  }
 
-export default Header;
+  renderModal(evt) {
+    this.props.toggleModal();
+  }
+
+  render() {
+    return(
+      <header>
+        <p>Welcome to Photo Album</p>
+        <strong
+          onClick={ this.renderModal }>&plus;</strong>
+        <OrderFormModal />
+      </header>
+    );
+  }
+};
+
+let mapStateToProps = (state) => ({
+  orderFormModal: state.orderFormModal
+});
+
+let mapDispatchToProps = (dispatch) => bindActionCreators({
+  toggleModal
+}, dispatch);
+
+export default connect(mapStateToProps, mapDispatchToProps)(Header);
