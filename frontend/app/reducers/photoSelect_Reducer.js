7143fd339d26ccba0f43bd96f3140b3c500b407e
Oliver Isenrich <IsenrichO@gmail.com>
2016-12-01 04:00:56 -0800

    init: Produce two new reducers to keep memory of selected items

--- frontend/app/reducers/photoSelect_Reducer.js
+++ frontend/app/reducers/photoSelect_Reducer.js
@@ -2,13 +2,17 @@
 import { SELECT_PHOTO } from '../actions/index';
 
 
+const toggleSelectedState = (photo, id = null) => { photo.selected = !photo.selected; };
+
 export default function photoSelect(state = {}, action) {
   switch (action.type) {
     case SELECT_PHOTO:
-      console.log(`Action <${action.type}> registered with payload <size: `, action.photo, '>');
-      // return action.photo;
-      !state.hasOwnProperty(action.photo['public_id'])
-        ? state[action.photo['public_id']] = action.photo
+      console.log(`Action <${action.type}> registered with payload <photo: `, action.photo, '>');
+      
+      const StatePhotoKey = action.photo['public_id'];
+      !state.hasOwnProperty(StatePhotoKey)
+        ? ( toggleSelectedState(action.photo), state[StatePhotoKey] = action.photo ) : state.hasOwnProperty(StatePhotoKey)
+        ? ( toggleSelectedState(action.photo), delete state[StatePhotoKey] )
         : null;
       console.log('PHOTO_SELECT_REDUCER:', state);
     default:
