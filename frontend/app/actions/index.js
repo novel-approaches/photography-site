08cb03887648abedb813f2b7c5be21ad3db4daba
Oliver Isenrich <IsenrichO@gmail.com>
2016-11-30 20:03:11 -0800

    feat: Register Redux action for ordering of photo

--- frontend/app/actions/index.js
+++ frontend/app/actions/index.js
@@ -3,6 +3,7 @@
 
 export const MODIFY_GRID_MARGINS = 'MODIFY_GRID_MARGINS';
 export const MODIFY_GRID_SIZE = 'MODIFY_GRID_SIZE';
+export const ORDER_PHOTO = 'ORDER_PHOTO';
 
 
 export const modifyGridMargins = (margins) => ({
@@ -13,4 +14,9 @@ export const modifyGridMargins = (margins) => ({
 export const modifyGridSize = (size) => ({
   type: MODIFY_GRID_SIZE,
   size
+});
+
+export const orderPhoto = (photo) => ({
+  type: ORDER_PHOTO,
+  photo
 });
