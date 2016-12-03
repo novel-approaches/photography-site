cd57b76c0ae97852c07d71c243e4925f3c8a3ddf
Oliver Isenrich <IsenrichO@gmail.com>
2016-12-03 01:01:25 -0800

    init: Create reducer to persist loading state

---
+++ frontend/app/reducers/ajaxSpinner_Reducer.js
@@ -0,0 +1,13 @@
+'use strict';
+import { SET_AJAX_SPINNER } from '../actions/index';
+
+
+export default function ajaxSpinner(state = false, action) {
+  switch (action.type) {
+    case SET_AJAX_SPINNER:
+      console.log(`Action <${action.type}> registered with payload <loading: ${action.loading}>`);
+      return action.loading;
+    default:
+      return state;
+  }
+};
