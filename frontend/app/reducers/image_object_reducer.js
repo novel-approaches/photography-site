3bbd7668f54f13302106f770c545367928f3aa32
Oliver Isenrich <IsenrichO@gmail.com>
2016-12-02 17:18:41 -0800

    fix: Return payload.data to access contents of GET request

--- frontend/app/reducers/image_object_reducer.js
+++ frontend/app/reducers/image_object_reducer.js
@@ -1,15 +1,13 @@
 'use strict';
 import { GET_IMAGE_OBJECT } from '../actions/index';
+import { SEED_DATA } from '../seed';
 
-import { DATA } from '../seed';
 
-export default function imageObject(state = DATA, action) {
+export default function imageObject(state = [], action) {
   switch (action.type) {
     case GET_IMAGE_OBJECT:
-      // console.log(`Action <${action.type}> registered with payload <size: ${action.size}>`);
-      console.log(action.payload);
-      // debugger;
-      return action.payload;
+      // console.log(`Action <${action.type}> registered with payload <payload: ${action.payload.data}>`);
+      return action.payload.data;
     default:
       return state;
   }
