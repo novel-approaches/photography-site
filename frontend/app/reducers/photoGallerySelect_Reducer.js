1e69c5cd7ef8888827383d827d3ffc256c25830f
Oliver Isenrich <IsenrichO@gmail.com>
2016-12-02 17:17:38 -0800

    chore: Correct references to SeedData

--- frontend/app/reducers/photoGallerySelect_Reducer.js
+++ frontend/app/reducers/photoGallerySelect_Reducer.js
@@ -1,21 +1,21 @@
 'use strict';
 import { TOGGLE_PHOTO_SELECTION_STATE } from '../actions/index';
+import { SEED_DATA } from '../seed';
 
-import { DATA } from '../seed';
 
-const DATA2 = DATA.reduce((memo, curr) => {
+const DATA2 = SEED_DATA.reduce((memo, curr) => {
   memo[curr.public_id] = curr;
   return memo;
 }, {});
 
-const SELECTION_STATES = DATA.reduce( (obj, item) => {
+const SELECTION_STATES = SEED_DATA.reduce( (obj, item) => {
   obj[item.public_id] = false;
   return obj;
 }, {});
 
 const togglePhotoSelectionState = (gallery, photo) => {
   let galleryPhoto = gallery[photo.public_id];
-  console.log('\n\nGALLERY PHOTO:', galleryPhoto);
+    // console.log('\n\nGALLERY PHOTO:', galleryPhoto);
   galleryPhoto.selected = !galleryPhoto.selected;
 };
 
@@ -25,7 +25,6 @@ export default function photoGallerySelect(state = SELECTION_STATES, action) {
       // console.log(`Action <${action.type}> registered with payload <photo: `, action.photo, '>');
       // let [photoID, photoObjEntry] = [action.photo.public_id, togglePhotoSelectionState(state, action.photo)];
       let photoID = action.photo.public_id;
-      console.log('PGSSSS', Object.assign(state, { [photoID]: !state[photoID] }));
       return Object.assign(state, { [photoID]: !state[photoID] });
     default:
       return state;
