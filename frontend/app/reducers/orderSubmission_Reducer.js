84dabef88008ee9dc1cb91d620ab6fd9a7a5c5cd
Oliver Isenrich <IsenrichO@gmail.com>
2016-12-03 19:41:21 -0800

    init: Accumulate item quantity in all sizes for all ordered photos

---
+++ frontend/app/reducers/orderSubmission_Reducer.js
@@ -0,0 +1,13 @@
+'use strict';
+import { SUBMIT_ORDER } from '../actions/index';
+
+
+export default function orderSubmission(state = {}, action) {
+  switch (action.type) {
+    case SUBMIT_ORDER:
+      console.log(`Action <${action.type}> registered with payload <payload: ${action.payload}` + '>');
+      return action.payload;
+    default:
+      return state;
+  }
+};
