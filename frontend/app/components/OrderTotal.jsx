d6ef57809a2643e498eebac20cffdbaaf406cda2
Oliver Isenrich <IsenrichO@gmail.com>
2016-12-03 19:39:28 -0800

    feat: Include component for handling order total

---
+++ frontend/app/components/OrderTotal.jsx
@@ -0,0 +1,21 @@
+'use strict';
+import React, { Component } from 'react';
+
+
+export default class OrderTotal extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <li
+        className="product-item">
+        <h5>Order Total: </h5>
+        <output>
+
+        </output>
+      </li>
+    );
+  }
+};
