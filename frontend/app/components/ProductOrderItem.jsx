d7a5802ba65c610a4edb0307b5ccffd13cc2405e
Oliver Isenrich <IsenrichO@gmail.com>
2016-12-03 19:36:13 -0800

    feat: Include product size forms in product order form

--- frontend/app/components/ProductOrderItem.jsx
+++ frontend/app/components/ProductOrderItem.jsx
@@ -1,15 +1,23 @@
 'use strict';
 import React from 'react';
 
+import ItemQuantityForm from './ItemQuantityForm';
+import TrashCanGlyph from '../constants/svg/TrashCanGlyph_SVG';
+
 
 const ProductOrderItem = ({ item }) => (
   <li
     className="product-item">
     <h5>{ item.public_id }</h5>
-    <img
-      src={ item.secure_url }
-      alt={ `Photograph thumbnail ${item.public_id}` }
-      height="75px" />
+    <div>
+      <img
+        src={ item.secure_url }
+        alt={ `Photograph thumbnail ${item.public_id}` }
+        height="75px" />
+      <ItemQuantityForm
+        photoID={ item.public_id } />
+      <TrashCanGlyph />
+    </div>
   </li>
 );
 
