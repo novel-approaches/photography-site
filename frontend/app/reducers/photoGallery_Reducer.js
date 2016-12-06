62c4bd85e2d4245b3c2672a3898b2fdfd3220b1f
Oliver Isenrich <IsenrichO@gmail.com>
2016-12-06 15:58:26 -0800

    chore: Delete and untrack unused Redux reducers

--- frontend/app/reducers/photoGallery_Reducer.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-export default function photoGallery(state = {}, action) {
-  // console.log(`Action <${action.type}> registered with payload <photo: `, action.photo, '>');
-  return state;
-};
