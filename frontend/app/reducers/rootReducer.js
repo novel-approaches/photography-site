b1a2c49cc5679b9a93f926ca348d3410da1930b8
Oliver Isenrich <IsenrichO@gmail.com>
2016-12-06 15:59:59 -0800

    chore: Remove imports to no longer existing reducers

--- frontend/app/reducers/rootReducer.js
+++ frontend/app/reducers/rootReducer.js
@@ -3,10 +3,8 @@ import { combineReducers } from 'redux';
 
 import gridMargins from './gridMargins_Reducer';
 import gridSize from './gridSize_Reducer';
-import photoOrder from './photoOrder';
 import orderFormModal from './orderFormModal_Reducer';
 import shoppingCart from './shoppingCart_Reducer';
-import photoGallery from './photoGallery_Reducer';
 import photoGallerySelect from './photoGallerySelect_Reducer';
 import imageObject from './image_object_reducer';
 import ajaxSpinner from './ajaxSpinner_Reducer';
@@ -17,10 +15,8 @@ import orderQuantity from './orderQuantity_Reducer';
 const rootReducer = combineReducers({
   gridMargins,
   gridSize,
-  photoOrder,
   orderFormModal,
   shoppingCart,
-  photoGallery,
   photoGallerySelect,
   imageObject,
   ajaxSpinner,
