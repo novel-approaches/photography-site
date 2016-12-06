9625d2f1a636d816223f971fa0635177ec7bc3ae
Daniel Cuperman <dacooperfish@gmail.com>
2016-12-06 13:30:32 -0800

    fixed gitignore

--- backend/auth.js
+++
@@ -1,9 +0,0 @@
-// module.exports = {
-//   user: 'postmaster@sandbox44ddf02b5433449d8f1e314174949ced.mailgun.org',
-//   pass: 'Athletics89'
-// }
-
-module.exports = {
-  user: 'clairesphotosoak',
-  pass: ''
-}
