f35d8f40d7484b6b60d0b71ad33e36c0bc64200e
Oliver Isenrich <IsenrichO@gmail.com>
2016-12-07 12:56:10 -0800

    chore: Tweak variable state names

--- frontend/app/reducers/photoGallerySelect_Reducer.js
+++ frontend/app/reducers/photoGallerySelect_Reducer.js
@@ -3,7 +3,7 @@ import { TOGGLE_PHOTO_SELECTION_STATE } from '../actions/index';
 import { SEED_DATA } from '../constants/SeedData';
 
 
-const DATA2 = SEED_DATA.reduce((memo, curr) => {
+const DATA = SEED_DATA.reduce((memo, curr) => {
   memo[curr.public_id] = curr;
   return memo;
 }, {});
@@ -15,7 +15,6 @@ const SELECTION_STATES = SEED_DATA.reduce( (obj, item) => {
 
 const togglePhotoSelectionState = (gallery, photo) => {
   let galleryPhoto = gallery[photo.public_id];
-    // console.log('\n\nGALLERY PHOTO:', galleryPhoto);
   galleryPhoto.selected = !galleryPhoto.selected;
 };
 
@@ -23,7 +22,6 @@ export default function photoGallerySelect(state = SELECTION_STATES, action) {
   switch (action.type) {
     case TOGGLE_PHOTO_SELECTION_STATE:
       // console.log(`Action <${action.type}> registered with payload <photo: `, action.photo, '>');
-      // let [photoID, photoObjEntry] = [action.photo.public_id, togglePhotoSelectionState(state, action.photo)];
       let photoID = action.photo.public_id;
       return Object.assign({}, state, { [photoID]: !state[photoID] });
     default:
