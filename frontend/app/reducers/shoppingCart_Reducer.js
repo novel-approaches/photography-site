a4dc99deb610cfaf62cee6b528b78649ec2dc5d2
Oliver Isenrich <IsenrichO@gmail.com>
2016-12-06 16:00:44 -0800

    fix: Make reducer immutable by applying Object.assign method on empty object, {}

--- frontend/app/reducers/shoppingCart_Reducer.js
+++ frontend/app/reducers/shoppingCart_Reducer.js
@@ -2,7 +2,7 @@
 import { ADD_TO_SHOPPING_CART } from '../actions/index';
 
 
-const toggleSelectedState = (photo) => Object.assign(photo, { selected: !photo.selected });
+const toggleSelectedState = (photo) => Object.assign({}, photo, { selected: !photo.selected });
 
 export default function photoSelect(state = {}, action) {
   switch (action.type) {
@@ -12,9 +12,9 @@ export default function photoSelect(state = {}, action) {
       const statePhotoKey = action.photo['public_id'],
             toggledPhoto = toggleSelectedState(action.photo);
       if (!state.hasOwnProperty(statePhotoKey)) {
-        return Object.assign(state, { [statePhotoKey]: toggledPhoto });
+        return Object.assign({}, state, { [statePhotoKey]: toggledPhoto });
       } else if (state.hasOwnProperty(statePhotoKey)) {
-        let newState = Object.assign(state);
+        let newState = Object.assign({}, state);
         delete newState[statePhotoKey];
         return newState;
       } else {
