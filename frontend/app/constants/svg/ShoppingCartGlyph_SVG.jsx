778ff2bd7ba766780e469c4a171507a90b43b705
Oliver Isenrich <IsenrichO@gmail.com>
2016-12-06 17:53:18 -0800

    init: Delegate shopping cart icon to pure functional component

---
+++ frontend/app/constants/svg/ShoppingCartGlyph_SVG.jsx
@@ -0,0 +1,42 @@
+'use strict';
+import React from 'react';
+
+
+const ShoppingCartGlyph = () => (
+  <svg
+    className="shopping-cart-glyph"
+    width="42.333336mm"
+    height="36.240959mm"
+    alt="Shopping cart glyph icon (Gray)."
+    version="1.1"
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 150.00001 128.41285">
+    <g transform="translate(-98.387356,-155.9648)">
+      <g transform="matrix(1.224659,0,0,1.224659,-22.103602,-63.88799)">
+        <g data-section="cart-contents">
+          <path d="m 149.76688,233.82602 1.94593,5.89318 54.43595,0 1.94125,-5.89318 z" />
+          <path d="m 144.38777,217.21498 1.75824,5.2845 65.48295,0 1.80465,-5.2845 z" />
+          <path d="m 139.07962,200.7078 1.63352,4.84099 76.45783,0 1.61697,-4.84099 z" />
+        </g>
+        <g data-section="underrack">
+          <path
+            d="m 102.19076,183.27201 20.3655,0 22.22335,66.67007 58.12438,0 m -58.88014,0.26518 -10.34031,15.25791 69.22045,0" />
+          <g data-section="wheels">
+            <ellipse
+              ry="6.5759969"
+              rx="6.5759974"
+              cy="277.80164"
+              cx="141.94836" />
+            <ellipse
+              cx="194.02501"
+              cy="277.80164"
+              rx="6.5759974"
+              ry="6.5759969" />
+          </g>
+        </g>
+      </g>
+    </g>
+  </svg>
+);
+
+export default ShoppingCartGlyph;
