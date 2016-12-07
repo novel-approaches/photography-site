7be545d84ea2338bb2fcb5888f5efb54d14b529e
Oliver Isenrich <IsenrichO@gmail.com>
2016-12-06 16:02:22 -0800

    fix: Make reducer immutable by applying Object.assign method on empty object, {}

--- frontend/app/reducers/orderQuantity_Reducer.js
+++ frontend/app/reducers/orderQuantity_Reducer.js
@@ -6,31 +6,28 @@ export default function orderQuantity(state = {}, action) {
   switch (action.type) {
     case CHANGE_ITEM_QUANTITY:
       // console.log(`Action <${action.type}> registered with payload <payload: `, action.payload, '>');
+      
       let { photoID, quantity, price } = action.payload,
           stateCopy;
-
       if (!state.hasOwnProperty(photoID)) {
-        stateCopy = Object.assign(state, {
+        stateCopy = Object.assign({}, state, {
           [photoID]: {
             quantity,
             price
           }
         });
       } else {
-        let stateQuantity = state[photoID].quantity;
-        let payloadQuantity = action.payload.quantity;
-        let newQ = Object.assign(state[photoID].quantity, action.payload.quantity);
-          console.log('\nNEWQ:', newQ);
+        let stateQuantity = state[photoID].quantity,
+            payloadQuantity = action.payload.quantity,
+            newQ = Object.assign({}, state[photoID].quantity, action.payload.quantity);
 
-        stateCopy = Object.assign(state, {
+        stateCopy = Object.assign({}, state, {
           [photoID]: {
             quantity: newQ,
             price
           }
         });
       }
-        // console.log('STATECOPY:', stateCopy);
-        // console.log('\n\nJSON', JSON.stringify(stateCopy));
       return stateCopy;
     default:
       return state;
