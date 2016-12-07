6f31b458e9d6aebe66bb564c329b03eb0f81895d
Oliver Isenrich <IsenrichO@gmail.com>
2016-12-07 12:57:49 -0800

    chore: Assign state default parameter value to 10

--- frontend/app/reducers/gridMargins_Reducer.js
+++ frontend/app/reducers/gridMargins_Reducer.js
@@ -2,7 +2,7 @@
 import { MODIFY_GRID_MARGINS } from '../actions/index';
 
 
-export default function gridMargins(state = 15, action) {
+export default function gridMargins(state = 10, action) {
   switch (action.type) {
     case MODIFY_GRID_MARGINS:
       // console.log(`Action <${action.type}> registered with payload <margins: ${action.margins}>`);
