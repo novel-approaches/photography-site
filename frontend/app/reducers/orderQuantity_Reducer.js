847b78bead72f8d70ca9a46419855ee4b8099998
travis.h.oneill@gmail.com <travis.h.oneill@gmail.com>
2016-12-08 13:45:09 -0800

    attempt to resolve

--- frontend/app/reducers/orderQuantity_Reducer.js
+++ frontend/app/reducers/orderQuantity_Reducer.js
@@ -6,23 +6,8 @@ export default function orderQuantity(state = {}, action) {
   switch (action.type) {
     case CHANGE_ITEM_QUANTITY:
       // console.log(`Action <${action.type}> registered with payload <payload: `, action.payload, '>');
-// <<<<<<< HEAD
-//
-//       // let photoID = action.payload.photoID,
-//       //     quantity = action
-//       let { photoID, quantity } = action.payload;
-//         // console.log('PAYLOAD:', photoID, quantity);
-//
-//       // !state.hasOwnProperty(photoID)
-//       //   ? Object.assign(state, { [photoID]:  })
-//       //   :
-//
-//       let stateCopy;
-// =======
-
       let { photoID, quantity, price } = action.payload,
           stateCopy;
-// >>>>>>> dev
       if (!state.hasOwnProperty(photoID)) {
         stateCopy = Object.assign({}, state, {
           [photoID]: {
@@ -31,35 +16,9 @@ export default function orderQuantity(state = {}, action) {
           }
         });
       } else {
-// <<<<<<< HEAD
-
-        let stateQuantity = state[photoID].quantity;
-          // console.log('\nSTATE QUANTITY:\t', stateQuantity);
-        let payloadQuantity = action.payload.quantity;
-          console.log('\nPAYLOAD QUANTITY:\t', payloadQuantity);
-        let newQuantityObj = {};
-
-        // for (let key in stateQuantity) {
-        //   if (payloadQuantity.hasOwnProperty(key)) {
-        //     newQuantityObj[key] = payloadQuantity[key];
-        //   } else {
-        //     newQuantityObj[key] = stateQuantity[key];
-        //   }
-        // }
-
-        let newQ = Object.assign(state[photoID].quantity, action.payload.quantity);
-          // console.log('\nNEWQ:', newQ);
-
-        // stateCopy = Object.assign(state, {
-        //   [photoID]: {
-        //     quantity: newQuantityObj
-        //   }
-        // });
-=======
         let stateQuantity = state[photoID].quantity,
             payloadQuantity = action.payload.quantity,
             newQ = Object.assign({}, state[photoID].quantity, action.payload.quantity);
->>>>>>> dev
 
         stateCopy = Object.assign({}, state, {
           [photoID]: {
@@ -68,11 +27,6 @@ export default function orderQuantity(state = {}, action) {
           }
         });
       }
-<<<<<<< HEAD
-        // console.log('STATECOPY:', stateCopy);
-        // console.log('\n\nJSON', JSON.stringify(stateCopy));
-=======
->>>>>>> dev
       return stateCopy;
     default:
       return state;
