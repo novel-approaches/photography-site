4d2e522821a9cf2fab9d7c72dc226cff5e861362
Oliver Isenrich <IsenrichO@gmail.com>
2016-12-07 21:48:06 -0800

    feat: Include 'Clear Cart' button

--- frontend/app/components/SubmitOrder.jsx
+++ frontend/app/components/SubmitOrder.jsx
@@ -1,26 +1,22 @@
 'use strict';
-import React, { Component } from 'react';
+import React from 'react';
 
 
-export default class SubmitOrder extends Component {
-  constructor(props) {
-    super(props);
-    this.onSub = this.onSub.bind(this);
-  }
+const SubmitOrder = ({ order, clearOrder, sendOrder }) => (
+  <div className="modal-actions">
+    <button
+      type="button"
+      name="clear-cart-btn"
+      onClick={ () => clearOrder() }>
+      Clear Cart
+    </button>
+    <button
+      type="submit"
+      name="submit-order-btn"
+      onClick={ () => sendOrder(order) }>
+      Submit Order
+    </button>
+  </div>
+);
 
-  onSub(evt) {
-    // evt.preventDefault();
-      console.log('\nORDER:', this.props.order);
-    this.props.sub(this.props.order);
-  }
-
-  render() {
-    return (
-      <button
-        type="submit"
-        onClick={ () => this.onSub() }>
-        Submit Order
-      </button>
-    );
-  }
-};
+export default SubmitOrder;
