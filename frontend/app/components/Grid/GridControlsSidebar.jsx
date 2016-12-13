5c584ad6ec21fcee854122028409568e2671b7ba
Oliver Isenrich <IsenrichO@gmail.com>
2016-12-13 10:52:03 -0800

    chore: Polish off sidebar

--- frontend/app/components/Grid/GridControlsSidebar.jsx
+++ frontend/app/components/Grid/GridControlsSidebar.jsx
@@ -7,24 +7,24 @@ import GridControls from './GridControls';
 
 
 const GridControlsSidebar = ({ gridMargins, gridSize, onSlideMarginsRange, onSlideSizeRange, sidebarToggleState, sidebarToolTip }) => {  
-  
   const toggleSidebar = function(evt) {
-    sidebarToggleState();
-
-    evt = evt || window.evt;
+    sidebarToggleState();     // Trigger passed down props function
+    evt = evt || window.evt;  // Fallback for legacy IE browsers
     const [$SIDEBAR, $TARG] = [$('#sidebar')[0], $(evt.currentTarget)],
           $GRID = $TARG.parent().next()[0],
           currWidth = parseFloat(window.getComputedStyle($SIDEBAR, null).getPropertyValue('width')),
           [width, marginLeft] = [( `${currWidth ? 0 : 10}vw` ), ( `${currWidth ? 0.5 : 10}vw` )];
     
+    // Stagger transition-delay property values for emphasis:
     [currWidth ? $SIDEBAR : $GRID, currWidth ? $GRID : $SIDEBAR]
       .forEach((el, index) => {
         $(el).css({ transitionDelay: `${index * 0.175}s` });
       });
 
+    // Employ new CSS property-value pairs:
     $($SIDEBAR).css({ width });
     $($GRID).css({ marginLeft });
-    $TARG.toggleClass('affixed');
+    $TARG.toggleClass('fix-left');
   };
   
   return (
