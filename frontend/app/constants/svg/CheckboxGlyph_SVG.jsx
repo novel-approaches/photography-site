11de367be3c663d40d4c426e1651c0901ed31197
Oliver Isenrich <IsenrichO@gmail.com>
2016-12-13 09:04:28 -0800

    chore: Decouple inline CSS transform style declarations to external SVG partial

--- frontend/app/constants/svg/CheckboxGlyph_SVG.jsx
+++ frontend/app/constants/svg/CheckboxGlyph_SVG.jsx
@@ -5,8 +5,6 @@ import React from 'react';
 const CheckboxGlyph = () => (
   <svg
     className="checkbox-glyph"
-    width="24px"
-    height="24px"
     version="1.1"
     xmlns="http://www.w3.org/2000/svg"
     viewBox="0 0 24 24">
