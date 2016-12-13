3409d8df1421a8dbfad74aedd234c7e61386631b
Oliver Isenrich <IsenrichO@gmail.com>
2016-12-13 10:50:27 -0800

    chore: Track moved file location

--- frontend/app/components/Grid/PhotoCheckbox.jsx
+++ frontend/app/components/Grid/PhotoCheckbox.jsx
@@ -1,7 +1,7 @@
 'use strict';
 import React from 'react';
 
-import CheckboxGlyph from '../constants/svg/CheckboxGlyph_SVG';
+import CheckboxGlyph from '../../constants/svg/CheckboxGlyph_SVG';
 
 
 const PhotoCheckbox = ({ photo, selectPhoto }) => (
